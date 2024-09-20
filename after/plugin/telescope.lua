7661c4232e691e191e7c7f3499bc00e4529abd2e
Georgy Bachakashvili <goshan@Georgys-MacBook-Pro.local>
2024-09-20 09:27:16 +0300

    beautified worktree config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,23 +1,17 @@
 require("telescope").load_extension("git_worktree")
 require("telescope").load_extension("live_grep_args")
-local builtin = require('telescope.builtin')
-local worktree = require('telescope').extensions.git_worktree
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "fuzzy-find project files" })
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function ()
-	builtin.grep_string()
-end, { desc = "grep string" })
-vim.keymap.set('n', '<leader>lf', function ()
-	builtin.live_grep()
-end, { desc = "grep project files" })
+local builtin = require("telescope.builtin")
+local worktree = require("telescope").extensions.git_worktree
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "fuzzy-find project files" })
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", builtin.grep_string, { desc = "grep string" })
+vim.keymap.set("n", "<leader>lf", builtin.live_grep, { desc = "grep project files" })
 
-vim.keymap.set('n', '<leader>wtl', function ()
-    worktree.git_worktrees()
-    -- <Enter> - switches to that worktree
-    -- <c-d> - deletes that worktree
-    -- <c-f> - toggles forcing of the next deletion
+vim.keymap.set("n", "<leader>wtl", function()
+	worktree.git_worktrees()
+	-- <Enter> - switches to that worktree
+	-- <c-d> - deletes that worktree
+	-- <c-f> - toggles forcing of the next deletion
 end, { desc = "list worktrees" })
 
-vim.keymap.set('n', '<leader>wtc', function ()
-    worktree.create_git_worktree()
-end, { desc = "create worktree" })
+vim.keymap.set("n", "<leader>wtc", worktree.create_git_worktree, { desc = "create worktree" })
