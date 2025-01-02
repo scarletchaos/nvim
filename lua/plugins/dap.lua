c3876a6f733c7d5b990e1bfe937020abe18fc944
Georgy Bachakashvili <goshan@MacBook-Pro.local>
2025-01-03 02:20:40 +0300

    tidyup

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,57 +2,63 @@ require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
 require('dap').set_log_level('TRACE')
 
 vim.schedule(function()
-    local ui = require("dapui")
     local dap = require("dap")
-    ui.setup()
+    local dapui = require("dapui")
 
+    -- DAP UI setup
+    dapui.setup()
+
+    -- Keymaps for DAP actions
     local keymap = vim.keymap.set
+    local mappings = {
+        { "<leader>td", dapui.toggle, "[t]oggle [d]apUI" },
+        { "<leader>bp", dap.toggle_breakpoint, "[b]reak[p]oint" },
+        { "<leader>bc", function()
+            local cond = vim.fn.input('Condition: ')
+            dap.toggle_breakpoint(cond)
+        end, "[b]reakpoint [c]onditional" },
+        { "<leader>dc", dap.continue, "[c]ontinue" },
+        { "<leader>dr", dap.repl.open, "[r]epl" },
+        { "<leader>so", dap.step_over, "[s]tep [o]ver" },
+        { "<leader>si", dap.step_into, "[s]tep [i]n" },
+    }
 
-    keymap("n", "<leader>td", ui.toggle, {noremap=true, desc="[t]oggle [d]apUI"})
-    keymap("n", "<leader>tbp", dap.toggle_breakpoint, {noremap=true, desc="[b]reak[p]oint"})
-    keymap("n", "<leader>tbc", function()
-        local cond = vim.fn.input('')
-        dap.toggle_breakpoint(cond)
-    end, {noremap=true, desc="[b]reakpoint [c]onditional"})
-    keymap("n", "<leader>dc", dap.continue, {noremap=true, desc="[c]ontinue"})
-    keymap("n", "<leader>dr", dap.repl.open, {noremap=true, desc="[r]epl"})
-    keymap("n", "<leader>so", dap.step_over, {noremap=true, desc="[s]tep [o]ver"})
-    keymap("n", "<leader>si", dap.step_into, {noremap=true, desc="[s]tep [i]n"})
-    -- If using the above, then `/path/to/venv/bin/python -m debugpy --version`
-    -- must work in the shell
-    dap.configurations.python = {}
+    for _, map in ipairs(mappings) do
+        keymap("n", map[1], map[2], { noremap = true, desc = map[3] })
+    end
 
-    table.insert(dap.configurations.python, {
-      type = 'python',
-      request = 'launch',
-      name = 'Debug current test',
-      program = '${workspaceFolder}/manage.py',
-      args = function()
-        local pattern = vim.fn.expand('%:t')
-        return { 'test', '--pattern', pattern, '--keepdb'}
-      end,
-      django = true,
-      justMyCode = false,
-    })
+    -- Python DAP configurations
+    dap.configurations.python = {
+        {
+            type = 'python',
+            request = 'launch',
+            name = 'Debug current test',
+            program = '${workspaceFolder}/manage.py',
+            args = function()
+                local pattern = vim.fn.expand('%:t')
+                return { 'test', '--pattern', pattern, '--keepdb' }
+            end,
+            django = true,
+            justMyCode = false,
+        },
+        {
+            type = 'python',
+            request = 'attach',
+            connect = {
+                host = '127.0.0.1',
+                port = 5678, -- Match the `debugpy` port
+            },
+            mode = 'remote',
+            name = 'Attach to running Django server',
+            justMyCode = false,
+        },
+    }
 
+    -- Python DAP adapter
     dap.adapters.python = {
         type = 'server',
         host = '127.0.0.1',
         port = 5678, -- Use the same port as in the `debugpy` command
     }
-    table.insert(dap.configurations.python, {
-        type = 'python',
-        request = 'attach',
-        connect = {
-            host = '127.0.0.1',
-            port = 5678, -- Match the `debugpy` port
-        },
-        mode = 'remote',
-        name = 'Attach to running Django server',
-        justMyCode = false,
-    })
 end)
-
-
-
 
