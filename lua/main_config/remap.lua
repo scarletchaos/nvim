c9dbb9abc69d5a85d02666439c0ef210cfed6a27
Georgy Bachakashvili <goshan@MacBook-Pro.local>
2025-01-02 22:36:31 +0300

    tidyup

--- lua/main_config/remap.lua
+++ lua/main_config/remap.lua
@@ -1,81 +1,77 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
+-- Leader key
 vim.g.mapleader = " "
-keymap("n", "<leader>pv", vim.cmd.Ex)
 
+-- General keymaps
+keymap("n", "<leader>pv", vim.cmd.Ex, { desc = ":Ex" })
 keymap("n", "<leader>-", vim.cmd.split, { desc = ":split" })
 keymap("n", "<leader>|", vim.cmd.vsplit, { desc = ":vsplit" })
--- Press jk fast to exit insert mode
+
+-- Insert mode shortcuts
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
+keymap("i", "<C-c>", "<Esc>")
 
--- Move text up and down with shift + j/k
+-- Visual mode shortcuts
 keymap("v", "J", ":m '>+1<CR>gv=gv")
 keymap("v", "K", ":m '<-2<CR>gv=gv")
-
--- Stay in indent mode after indenting
 keymap("v", "<", "<gv^", opts)
 keymap("v", ">", ">gv^", opts)
 
--- Replace next \n with space
+-- Navigation improvements
 keymap("n", "J", "mzJ`z")
 keymap("n", "<C-d>", "<C-d>zz")
 keymap("n", "<C-u>", "<C-u>zz")
-
--- Next/prev item in search (with centering)
 keymap("n", "n", "nzzzv")
 keymap("n", "N", "Nzzzv")
 
--- Paste over something without buffering that something
+-- Clipboard and paste
 keymap("x", "<leader>p", [["_dP]], { desc = "Paste without buffering" })
-
--- Yank something into + register
 keymap({ "n", "v" }, "<leader>y", [["+y]])
 keymap("n", "<leader>Y", [["+Y]])
-
 keymap({ "n", "v" }, "<leader>d", [["_d]], { desc = "d without buffering" })
 keymap({ "n", "v" }, "<leader>c", [["_c]], { desc = "c without buffering" })
 
--- This is going to get me cancelled
-keymap("i", "<C-c>", "<Esc>")
-
+-- Command quickfix navigation
 keymap("n", "Q", "<nop>")
-
 keymap("n", "<C-k>", "<cmd>cnext<CR>zz")
 keymap("n", "<C-j>", "<cmd>cprev<CR>zz")
 keymap("n", "<leader>k", "<cmd>lnext<CR>zz")
 keymap("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace under cursor" })
+-- Text replacements
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace under cursor" })
 keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- Configuration editing
 keymap("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/goshan/packer.lua<CR>")
-
 keymap("n", "<leader><leader>", function()
 	vim.cmd("so")
 end, { desc = "source this" })
 
-keymap("n", "<leader>ga", "<cmd>G add %<CR>", { desc = "git add this" })
-keymap("n", "<leader>gc", "<cmd>G commit<CR>", { desc = "git commit" })
-keymap("n", "<leader>gk", "<cmd>G push<CR>", { desc = "git push" })
-keymap("n", "<leader>gj", "<cmd>G pull --rebase<CR>", { desc = "git pull --rebase" })
-keymap("n", "<leader>gJ", "<cmd>G pull<CR>", { desc = "git pull" })
-keymap("n", "<leader>gl", "<cmd>G log --oneline --decorate --graph --all<CR>", { desc = "git log but fancy" })
+-- Git integration
+-- keymap("n", "<leader>ga", "<cmd>G add %<CR>", { desc = "git add this" })
+-- keymap("n", "<leader>gc", "<cmd>G commit<CR>", { desc = "git commit" })
+-- keymap("n", "<leader>gk", "<cmd>G push<CR>", { desc = "git push" })
+-- keymap("n", "<leader>gj", "<cmd>G pull --rebase<CR>", { desc = "git pull --rebase" })
+-- keymap("n", "<leader>gJ", "<cmd>G pull<CR>", { desc = "git pull" })
+-- keymap("n", "<leader>gl", "<cmd>G log --oneline --decorate --graph --all<CR>", { desc = "git log but fancy" })
+keymap("n", "<leader>gs", vim.cmd.Git, { desc = "open fugitive pane" })
 
-keymap("n", "<leader>db", "<cmd>DBUIToggle<CR>", { desc = "Open DadBod" })
--- Navigate subwindows with ctrl+hjkl
+-- Window navigation
 keymap("n", "<C-h>", "<C-w>h")
 keymap("n", "<C-j>", "<C-w>j")
 keymap("n", "<C-k>", "<C-w>k")
 keymap("n", "<C-l>", "<C-w>l")
 
+-- Plugins
 -- Tagbar
 keymap("n", "<leader>tb", "<cmd>TagbarToggle<CR>")
-
+-- Oil
 keymap("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
--- Lua
+-- Trouble
 keymap("n", "<leader>xx", function()
 	require("trouble").toggle()
 end, { desc = "toggle error view" })
@@ -95,6 +91,19 @@ keymap("n", "gR", function()
 	require("trouble").toggle("lsp_references")
 end, { desc = "toggle lsp references" })
 
+-- Toggle line numbers
+function ToggleLineNumbers()
+    if vim.wo.relativenumber then
+        vim.wo.relativenumber = false
+        vim.wo.number = true
+    else
+        vim.wo.relativenumber = true
+        vim.wo.number = true
+    end
+end
+keymap("n", "<leader>rl", ToggleLineNumbers, { desc = "toggle relative line numbers" })
+
+-- Ruff toggle
 local ruff_active = true
 vim.keymap.set("n", "<leader>tr", function()
 	ruff_active = not ruff_active
@@ -105,9 +114,10 @@ vim.keymap.set("n", "<leader>tr", function()
 	end
 end)
 
+-- Run script
 keymap("n", "<leader>rp", "<cmd>!./run.sh<CR>")
 
-
+-- Curl
 keymap("n", "<leader>tc", function() require("curl").open_curl_tab() end, { desc = "Open a curl tab scoped to CWD" })
 keymap("n", "<leader>to", function() require("curl").open_global_tab() end, { desc = "Open a curl tab with global scope" })
 keymap("n", "<leader>tcsc", function() require("curl").create_scoped_collection() end, { desc = "Create or open a collection with a name from user input" })
@@ -115,5 +125,3 @@ keymap("n", "<leader>tcgc", function() require("curl").create_global_collection(
 keymap("n", "<leader>tfsc", function() require("curl").pick_scoped_collection() end, { desc = "Choose a scoped collection and open it" })
 keymap("n", "<leader>tfgc", function() require("curl").pick_global_collection() end, { desc = "Choose a global collection and open it" })
 
-
-keymap("n", "<leader>gs", vim.cmd.Git, { desc = "open fugitive pane"})
--- lua/main_config/set.lua
+++ lua/main_config/set.lua
@@ -1,33 +1,42 @@
-vim.opt.guicursor =
-	"n-v-c-sm:block-blinkwait700-blinkoff400-blinkon250,i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250-,r-cr-o:hor20,a:Cursor/Cursor"
+-- Cursor settings
+vim.opt.guicursor = "n-v-c-sm:block-blinkwait700-blinkoff400-blinkon250,i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250-,r-cr-o:hor20,a:Cursor/Cursor"
+
+-- Line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- Tab and indentation settings
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- Text wrapping
 vim.opt.wrap = false
 
+-- Backup and undo settings
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Search settings
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Visual settings
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+
+-- Filename settings
 vim.opt.isfname:append("@-@")
 
+-- Performance
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
-vim.opt.langmap =
-"йЙцЦуУкКеЕнНгГшШщЩзЗхХъЪфФыЫвВаАпПрРоОлЛдДжЖэЭяЯчЧсСмМиИтТьЬбБюЮёË;qQwWeErRtTyYuUiIoOpP[{]}aAsSdDfFgGhHjJkKlL\\;\\:\'\"zZxXcCvVbBnNmM\\,\\<\\.\\>`~"
+-- Language mapping
+vim.opt.langmap = "йЙцЦуУкКеЕнНгГшШщЩзЗхХъЪфФыЫвВаАпПрРоОлЛдДжЖэЭяЯчЧсСмМиИтТьЬбБюЮёË;qQwWeErRtTyYuUiIoOpP[{]}aAsSdDfFgGhHjJkKlL\\;\\:\'\"zZxXcCvVbBnNmM\\,\\<\\.\\>`~"
 
