d8f02cda19b53089912020d1787ca270268aba8c
Georgy Bachakashvili <goshan@MacBook-Pro.local>
2025-01-03 12:16:41 +0300

    added comments

--- lua/main_config/lazy.lua
+++ lua/main_config/lazy.lua
@@ -17,7 +17,7 @@ local lazy = require("lazy")
 lazy.setup({
 	-- COLORSCHEME
 	{
-		"rebelot/kanagawa.nvim",
+		"rebelot/kanagawa.nvim", -- A beautiful colorscheme inspired by Japanese art
 		name = "kanagawa",
 		config = function()
 			vim.cmd("colorscheme kanagawa")
@@ -26,9 +26,7 @@ lazy.setup({
 
 	-- FILE EXPLORATION
 	{
-		"stevearc/oil.nvim",
-		---@module 'oil'
-		---@type oil.SetupOpts
+		"stevearc/oil.nvim", -- Simplified file navigation and exploration
 		opts = {},
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		config = function()
@@ -38,7 +36,7 @@ lazy.setup({
 
 	-- TELESCOPE AND EXTENSIONS
 	{
-		"nvim-telescope/telescope.nvim",
+		"nvim-telescope/telescope.nvim", -- Powerful fuzzy finder for files, buffers, and more
 		tag = "0.1.8",
 		event = "VeryLazy",
 		dependencies = { "nvim-telescope/telescope-live-grep-args.nvim", "nvim-lua/plenary.nvim" },
@@ -46,89 +44,89 @@ lazy.setup({
 			require("plugins.telescope")
 		end,
 	},
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- Fuzzy finder acceleration with FZF
+
+	-- GIT WORKFLOW
 	{
-		"ThePrimeagen/git-worktree.nvim",
+		"ThePrimeagen/git-worktree.nvim", -- Manage and switch between Git worktrees
 		config = function()
 			require("plugins.worktree")
 		end,
 	},
 
-	-- LSP, AUTOCOMPLETION AND AI
-	{ "williamboman/mason.nvim" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-path" },
-	{ "hrsh7th/cmp-cmdline" },
+	-- LSP, AUTOCOMPLETION, AND AI
+	{ "williamboman/mason.nvim" }, -- Manage LSP servers, linters, and formatters
+	{ "hrsh7th/cmp-nvim-lsp" }, -- LSP-based autocompletion
+	{ "hrsh7th/cmp-buffer" }, -- Autocompletion from buffer content
+	{ "hrsh7th/cmp-path" }, -- Autocompletion for file paths
+	{ "hrsh7th/cmp-cmdline" }, -- Autocompletion for Vim command-line
 	{
-		"hrsh7th/nvim-cmp",
+		"hrsh7th/nvim-cmp", -- Main autocompletion plugin
 		config = function()
 			require("plugins.cmp")
 		end,
 	},
 	{
-		"VonHeikemen/lsp-zero.nvim",
+		"VonHeikemen/lsp-zero.nvim", -- Simplified LSP setup with sensible defaults
 		branch = "v3.x",
 		dependencies = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ "williamboman/mason.nvim" }, -- Optional
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
-
-			-- Autocompletion
-			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "neovim/nvim-lspconfig" }, -- Core LSP configurations
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "L3MON4D3/LuaSnip" }, -- Snippet engine for autocompletion
 		},
 	},
 	{
-		"zbirenbaum/copilot.lua",
+		"zbirenbaum/copilot.lua", -- GitHub Copilot integration for AI-powered autocompletions
 		cmd = "Copilot",
 		event = "InsertEnter",
 		config = function()
-            require("plugins.copilot")
+			require("plugins.copilot")
 		end,
 	},
+
 	-- TREESITTER
 	{
-		"nvim-treesitter/nvim-treesitter",
+		"nvim-treesitter/nvim-treesitter", -- Syntax highlighting and code parsing
 		build = ":TSUpdate",
 		config = function()
 			require("plugins.treesitter")
 		end,
 	},
-	{ "nvim-treesitter/playground" },
+	{ "nvim-treesitter/playground" }, -- Inspect Treesitter syntax nodes interactively
 
 	-- GIT INTEGRATION
-	{ "airblade/vim-gitgutter" },
+	{ "airblade/vim-gitgutter" }, -- Show Git diff indicators in the gutter
 	{
-		"tpope/vim-fugitive",
+		"tpope/vim-fugitive", -- Git commands within Vim
 		config = function()
 			require("plugins.fugitive")
 		end,
 	},
 
 	-- STATUS LINE AND ICONS
-	{ "nvim-tree/nvim-web-devicons" },
-    { "echasnovski/mini.icons" },
+	{ "nvim-tree/nvim-web-devicons" }, -- File type icons for Neovim
+	{ "echasnovski/mini.icons" }, -- Additional icon support for plugins
 	{
-		"nvim-lualine/lualine.nvim",
+		"nvim-lualine/lualine.nvim", -- Highly configurable status line
 		config = function()
 			require("plugins.lualine")
 		end,
 	},
 
 	-- MULTIPLE CURSORS
-	{ "mg979/vim-visual-multi" },
+	{ "mg979/vim-visual-multi" }, -- Support for multiple cursors like VS Code
 
 	-- NAVIGATION
 	{
-		"ggandor/leap.nvim",
+		"ggandor/leap.nvim", -- Fast and intuitive motions within files
 		dependencies = { "tpope/vim-repeat" },
 		config = function()
 			require("leap").create_default_mappings()
 		end,
 	},
 	{
-		"theprimeagen/harpoon",
+		"theprimeagen/harpoon", -- Efficient file and buffer navigation
 		event = "VeryLazy",
 		config = function()
 			require("plugins.harpoon")
@@ -137,38 +135,38 @@ lazy.setup({
 
 	-- DEBUGGING
 	{
-		"rcarriga/nvim-dap-ui",
+		"rcarriga/nvim-dap-ui", -- Debug Adapter Protocol UI for debugging
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
 		config = function()
 			require("plugins.dap")
 		end,
 	},
-	{ "mfussenegger/nvim-dap-python" },
-	{ "theHamsta/nvim-dap-virtual-text" },
+	{ "mfussenegger/nvim-dap-python" }, -- Python debugging support
+	{ "theHamsta/nvim-dap-virtual-text" }, -- Inline virtual text for DAP
 
 	-- UTILITIES
 	{
-		"mbbill/undotree",
+		"mbbill/undotree", -- Visualize and manage undo history
 		config = function()
 			require("plugins.undotree")
 		end,
 	},
 	{
-		"folke/which-key.nvim",
+		"folke/which-key.nvim", -- Display available keymaps on pressing `<leader>`
 		config = function()
 			vim.o.timeout = true
 			vim.o.timeoutlen = 300
 			require("which-key").setup({})
 		end,
 	},
-	{ "tpope/vim-dadbod" },
-	{ "kristijanhusak/vim-dadbod-ui" },
-	{ "kristijanhusak/vim-dadbod-completion" },
+	{ "tpope/vim-dadbod" }, -- Database exploration and management
+	{ "kristijanhusak/vim-dadbod-ui" }, -- UI for vim-dadbod
+	{ "kristijanhusak/vim-dadbod-completion" }, -- Autocompletion for database queries
 
 	-- TEXT MANIPULATION
-	{ "tpope/vim-commentary" },
+	{ "tpope/vim-commentary" }, -- Comment and uncomment code
 	{
-		"kylechui/nvim-surround",
+		"kylechui/nvim-surround", -- Manage surrounding text (quotes, brackets, etc.)
 		version = "*",
 		event = "VeryLazy",
 		config = function()
@@ -178,25 +176,23 @@ lazy.setup({
 
 	-- CHUNK HIGHLIGHTING
 	{
-		"shellRaining/hlchunk.nvim",
+		"shellRaining/hlchunk.nvim", -- Highlight indentation levels and code chunks
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			require("plugins.hlchunk")
 		end,
 	},
+
 	-- LINTERS AND FORMATTERS
 	{
-		"mfussenegger/nvim-lint",
-		event = {
-			"BufReadPre",
-			"BufNewFile",
-		},
+		"mfussenegger/nvim-lint", -- Linting support for multiple languages
+		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			require("plugins.lint")
 		end,
 	},
 	{
-		"stevearc/conform.nvim",
+		"stevearc/conform.nvim", -- Formatting engine for various file types
 		event = "VeryLazy",
 		config = function()
 			require("plugins.conform")
@@ -205,19 +201,19 @@ lazy.setup({
 
 	-- MISC
 	{
-		"folke/trouble.nvim",
-		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		"folke/trouble.nvim", -- Pretty diagnostics and references list
+		opts = {}, -- Default options
 		cmd = "Trouble",
 	},
-    {
-        import = "plugins.snacks",
-    },
-	{ "mistricky/codesnap.nvim", build = "make" },
-	{ "ThePrimeagen/vim-be-good" },
-	{ "kevinhwang91/nvim-bqf" },
-	{ "preservim/tagbar" },
 	{
-		"windwp/nvim-autopairs",
+		import = "plugins.snacks", -- Snacks.nvim for enhanced UI features
+	},
+	{ "mistricky/codesnap.nvim", build = "make" }, -- Generate code snapshots for sharing
+	{ "ThePrimeagen/vim-be-good" }, -- Fun plugin for improving Vim motions
+	{ "kevinhwang91/nvim-bqf" }, -- Better quickfix window UI
+	{ "preservim/tagbar" }, -- Show tags (functions, variables) in a sidebar
+	{
+		"windwp/nvim-autopairs", -- Automatically close brackets, quotes, etc.
 		event = "InsertEnter",
 		config = true,
 	},
